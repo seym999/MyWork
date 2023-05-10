154e4b2a9ef090147ef4275df61efd831d4cbdfb
seyma <seymakara888@gmail.com>
2023-05-10 15:24:33 +0300

    V02

--- src/main/java/homework/Homework04.java
+++ src/main/java/homework/Homework04.java
@@ -1,4 +1,26 @@
 package homework;
 
+import java.util.Scanner;
+
 public class Homework04 {
+    public static void main(String[] args) {
+        //homework
+        //1) Kullanicidan aldiginiz 3 tane sayinin ortalamasini bulan kodu yaziniz
+
+        Scanner input =  new Scanner(System.in);
+        System.out.println("Lütfen 1.sayiyi giriniz ");
+        double sayi1=input.nextDouble();
+        System.out.println("Lütfen 2.sayiyi giriniz");
+        double sayi2=input.nextDouble();
+        System.out.println("Lütfen 3.sayiyi giriniz");
+        double sayi3=input.nextDouble();
+
+        double ortalama=(sayi1+sayi2+sayi3)/3;
+        System.out.println("Girmiş oldugunuz 3 sayinin ortalamasi = " +ortalama + " dir..");
+
+
+
+
+
+    }
 }
--- src/main/java/homework/Homework05.java
+++ src/main/java/homework/Homework05.java
@@ -1,26 +1,26 @@
 package homework;
 
+
+
 import java.util.Scanner;
 
 public class Homework05 {
     public static void main(String[] args) {
-        //homework
-        //1) Kullanicidan aldiginiz 3 tane sayinin ortalamasini bulan kodu yaziniz
+        //2) Kullanicidan aldiginiz 3 basamakli bir sayinin rakamlari toplamini bulunuz
+        Scanner input = new Scanner(System.in);
+        System.out.println("Lutfen 3 basamakli bir sayi giriniz...");
+        int sayi = input.nextInt();
 
-        Scanner input =  new Scanner(System.in);
-        System.out.println("Lütfen 1.sayiyi giriniz ");
-        double sayi1=input.nextDouble();
-        System.out.println("Lütfen 2.sayiyi giriniz");
-        double sayi2=input.nextDouble();
-        System.out.println("Lütfen 3.sayiyi giriniz");
-        double sayi3=input.nextDouble();
+        int birlerBasamagi = sayi % 10;
+        sayi = sayi / 10;
+        int onlarBasamagi = sayi % 10;
+        sayi = sayi / 10;
+        int yuzlerBasamagi = sayi % 10;
 
-        double ortalama=(sayi1+sayi2+sayi3)/3;
-        System.out.println("Girmiş oldugunuz 3 sayinin ortalamasi = " +ortalama + " dir..");
+        sayi = input.nextInt();
 
+        int basamakToplami = birlerBasamagi + onlarBasamagi + yuzlerBasamagi;
 
-
-
-
+        System.out.println("Girmiş oldugunuz " + sayi + " sayisinin rakamlari tolami = " + basamakToplami + " dir...");
     }
 }
